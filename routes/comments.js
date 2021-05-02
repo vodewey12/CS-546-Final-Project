a3d3ed90a4308b633af7a0c84a237c39c2721675
songhany <ysh961008@yeah.net>
2021-05-01 21:18:12 -0400

    1st submit /routes/comments.js

---
+++ routes/comments.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const { getCommentByCommentId } = require('../data/comments');
+const userData = data.reviews;  // ../data/users.js
+const postData = data.books;  // ../data/posts.js
+const commentData = data.comments;  // ../data/comments.js
+
+
+router.get('/', async (req, res) => {  // return an array of all comments
+  try {
+    const commentOfPost= await commentData.getAllComments();
+    res.status(200).json(commentOfPost)
+  } catch (e) {
+    res.status(404).json({ message: 'getAllComments() fails' });
+    return;
+  }
+});
+
+
+router.get('/:id', async (req, res) => {  // /:id is commentId
+  try {
+    const commentOfPost= await commentData.getCommentByCommentId(req.params.id)   // req.params.id is commentId
+    res.status(200).json(commentOfPost)
+  } catch (e) {
+    res.status(404).json({ message: 'getCommentByCommentId(req.params.id) fails' });
+    return;
+  }
+});
+
+
+router.post('/', async (req, res) => {
+  const aCommentData = req.body;  // get data that need to be posted
+
+  if (!aCommentData.comContent || typeof aCommentData.comContent != 'string' ) {
+    res.status(400).json({ error: 'You must provide comment Content in string type' });
+    return;
+  }
+  if (!aCommentData.userName || typeof aCommentData.userName != 'string' ) {
+    res.status(400).json({ error: 'You must provide userName in string type' });
+    return;
+  }
+
+  try {  // Json is valid and comment can be created successful
+    const { comContent, userName } = aCommentData;  
+    const commentThatPost = await commentData.createComments() // create a comment
+    res.status(200).json(commentThatPost);
+  } catch (e) {
+    res.status(500).json({ error: "createComments() fails" })
+  }
+});
+
+
+router.patch('/:id', async (req, res) => {
+  const updatedComment = req.body;
+  if (!updatedComment.comContent) {
+    res.status(400).json({ error: 'You must comment content that you want to update' });
+    return; 
+  }
+
+  let updatedObject = {};
+  try {
+    const currentComment = await commentData.getCommentByCommentId(req.params.id);
+    // compare patchComment with currentComment
+    if (updatedComment.comContent && updatedComment.comContent !== currentComment.comContent)
+      updatedObject.comContent = updatedComment.comContent;
+  } catch (e) {
+    res.status(404).json({ error: 'Patch failed' });
+    return;
+  }
+
+  if (Object.keys(updatedObject).length !== 0) {  //there is updated info
+    try {
+      const editCom = await commentData.editComment(req.params.id, updatedObject);
+      res.status(200).json(editCom);
+    } catch (e) {
+      res.status(500).json({ error: 'editComment() fails' });
+    }
+  } else {
+    res.status(400).json({error: 'No comment content have been changed, so no update has occurred'});
+  }
+});
+
+
+router.delete('/:id', async (req, res) => {
+  if (!req.params.id) {
+    res.status(400).json({ error: 'You must supply an commentId to delete' });
+    return;
+  }
+
+  try {
+    await commentData.getCommentByCommentId(req.params.id);
+  } catch (e) {
+    res.status(404).json({ error: 'Comment not found with this id' });
+    return;
+  }
+
+  try {
+    await commentData.deleteComment(req.params.id);
+    res.status(200).json({ 'commentId': req.params.id, "deleted": true });
+  } catch (e) {
+    res.status(500).json({error: 'deleteComment() fails' });
+  }
+});
+
+module.exports = router;
