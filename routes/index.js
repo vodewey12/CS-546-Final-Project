95f1f1b86fb0f95eeab7323e72927ecba05e39f9
vodewey12 <voduy12@gmail.com>
2021-05-08 13:36:26 -0400

    oops accidentally deteled a bracket during merge, its been fixed

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,16 @@
-
 const commentRoutes = require("./comments");
 const userRoutes = require("./users");
 const postRoutes = require("./posts");
-
 
 const constructorMethod = (app) => {
   app.use("/users", userRoutes);
   app.use("/comments", commentRoutes);
   app.use("/posts", postRoutes);
-  app.use('/profile', profileRoutes);  // need this router to show user profile
-  app.use('/', profileRoutes);
-  app.use('*', (req, res) => {
-        res.sendStatus(404);
-    });
+  app.use("/profile", profileRoutes); // need this router to show user profile
+  app.use("/", profileRoutes);
+  app.use("*", (req, res) => {
+    res.sendStatus(404);
+  });
+};
 
 module.exports = constructorMethod;
