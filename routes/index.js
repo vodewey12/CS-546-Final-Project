374476b0cd8e0ff1ba83dd79d9ff21dd480cde43
songhany <ysh961008@yeah.net>
2021-05-09 00:52:37 -0400

    fix typo

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const constructorMethod = (app) => {
   app.use("/posts", postRoutes);
   app.use("/profile", profileRoutes); // need this router to show user profile
   app.use("/dashboard", dashboardRoutes);
-  app.use("/", profileRoutes);
+
   app.use("*", (req, res) => {
     res.sendStatus(404);
   });
