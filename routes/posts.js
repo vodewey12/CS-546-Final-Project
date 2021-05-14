8f1749fde0ee67ce6ebc8bfc212b04acf59d2fbb
vodewey12 <voduy12@gmail.com>
2021-05-13 22:21:39 -0400

    added redirect after solve

--- routes/posts.js
+++ routes/posts.js
@@ -248,6 +248,7 @@ router.post("/resolve", async (req, res) => {
   try {
     await postData.resolvePosts(postId);
     await commentData.markCommentSol(commentId);
+    res.redirect("/");
   } catch (e) {
     res.status(404).json({ error: e });
   }
