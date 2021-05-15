d3af877e2fcfb8fb5dfc6c6e943c15d5d0fddc77
vodewey12 <voduy12@gmail.com>
2021-05-14 20:14:29 -0400

    results now prints if no posts were found

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -17,31 +17,35 @@
         url: "http://localhost:3000/posts/search/" + inputVal,
       }).then((res) => {
         $posts.hide();
-        res.forEach((data) => {
-          var list = $("<li>");
-          var a = $("<a id=" + data._id + " href=/posts/" + data._id + " >");
-          if (data.resolvedStatus) {
-            a.text(
-              "[Resolved] " +
-                data.title +
-                " posted by " +
-                data.userName +
-                " on " +
-                moment(data.postTime).format("MM/DD/YYYY")
-            );
-          } else {
-            a.text(
-              "[Unresolved] " +
-                data.title +
-                " posted by " +
-                data.userName +
-                " on " +
-                moment(data.postTime).format("MM/DD/YYYY")
-            );
-          }
-          list.append(a);
-          $posts.append(list);
-        });
+        if (res.length == 0) {
+          $posts.append($("<h2>No posts were found</h2>"));
+        } else {
+          res.forEach((data) => {
+            var list = $("<li>");
+            var a = $("<a id=" + data._id + " href=/posts/" + data._id + " >");
+            if (data.resolvedStatus) {
+              a.text(
+                "[Resolved] " +
+                  data.title +
+                  " posted by " +
+                  data.userName +
+                  " on " +
+                  moment(data.postTime).format("MM/DD/YYYY")
+              );
+            } else {
+              a.text(
+                "[Unresolved] " +
+                  data.title +
+                  " posted by " +
+                  data.userName +
+                  " on " +
+                  moment(data.postTime).format("MM/DD/YYYY")
+              );
+            }
+            list.append(a);
+            $posts.append(list);
+          });
+        }
         $createCard.hide();
         $posts.show();
       });
