a52c5c57d0a6361cdda3df47e860eab7e8e491ae
Marjan Chowdhury <marjanchowdhury@marjans-mbp.lan>
2021-05-15 19:39:39 -0400

    created homepage

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ router.get("/", async (req, res) => {
     // user is authenticated
     //res.redirect('/home');
     // res.status(200).json({ message: 'User is authenticated.' }); // should be redirected to home/dashboard once built
-    res.redirect(`/posts`); // if user already log in, when they go to login page, they should redirect profile page instead of seeing login form
+    res.render("dashboard/home",{title: 'Home',partial: "dashboard_js_script"}); // if user already log in, when they go to login page, they should redirect profile page instead of seeing login form
     return;
   } else {
     res.render("auth/login", {
